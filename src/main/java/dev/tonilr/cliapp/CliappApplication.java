6b2c93783af2b8bb2ccc90be134f3cfbb61dbd05
Toni <tonilupianezroman@gmail.com>
2024-04-20 17:04:11 +0200

    i18n

--- src/main/java/dev/tonilr/cliapp/CliappApplication.java
+++ src/main/java/dev/tonilr/cliapp/CliappApplication.java
@@ -1,8 +1,11 @@
 	package dev.tonilr.cliapp;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -13,6 +16,11 @@ import dev.tonilr.cliapp.client.DadJokeClient;
 public class CliappApplication {
 
 	public static void main(String[] args) {
+		
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasenames("lang/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    System.out.println(messageSource.getMessage("hello", null, Locale.ITALIAN));
 		SpringApplication.run(CliappApplication.class, args);
 	}
 
